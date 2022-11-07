223485da91e8b83b45c661e20b716d20e5701f3c
elpapu77 <sebastianarevaloboj@gmail.com>
2022-11-07 08:36:08 -0500

    commit inicial

---
+++ src/components/componente_a/componente-a/componente-a.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ComponenteAComponent } from './componente-a.component';
+
+
+
+@NgModule({
+  declarations: [ComponenteAComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ComponenteAModule { }
